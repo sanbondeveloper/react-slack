b583c324794905e6a9b1ea9b1a33e85fd515fe96
sanbondeveloper <sanbondeveloper@gmail.com>
2023-11-26 13:13:12 +0900

    🌱 폴더 구조 및 리액트 라우터 설정

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Login from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>App!</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="login" />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;
